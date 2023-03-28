be3840241743ed62648e6fbea56ef4313ea59b53
riossigron <123623632+riossigron@users.noreply.github.com>
2023-03-28 16:28:03 +0800

    Add files via upload

---
+++ rios_multiplicationTable.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Multiplication</title>
+</head>
+<body>
+    
+    <center>
+    <br>
+    <h2>Multiplication Table of 5:</h2>
+    <?php
+        $number = 5;
+
+    for($i=1; $i<=10; $i++){
+            echo $number ." x ". $i ." = ". $number * $i ."<br><br>";
+        // echo "Sigron";
+        }
+    ?>
+    </center>
+</body>
+</html>
